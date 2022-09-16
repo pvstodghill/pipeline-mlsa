a8b8e1edcb53187c60113eb047c3ec17690ffbb6
Paul Stodghill <paul.stodghill@usda.gov>
2022-09-16 15:14:35 -0400

    Muscle has an odd way of specifying threads.

--- doit2-afas.bash
+++ doit2-afas.bash
@@ -12,8 +12,12 @@ echo 1>&2 '# Align sequences'
 rm -rf ${AFAS}
 mkdir -p ${AFAS}
 
+# v-- this is needed for multi-threading
+export OMP_NUM_THREADS=${THREADS}
+
 for GENE in ${GENES} ; do
     echo 1>&2 "## $GENE"
+    #      v-- this is not required for multi-threading
     muscle -threads ${THREADS} -align ${SEQS}/${GENE}.fasta -output ${AFAS}/${GENE}.afa
 done
 
